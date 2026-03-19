e24b342f62166e2c9826a71b4f6cdad2ef1c145e
Fergus Nally <12160@elizabethcollege.gg>
2026-03-19 11:36:25 +0000

    Bump version from 3.12 to 3.13

--- stub.js
+++ stub.js
@@ -1,7 +1,7 @@
    // ==UserScript==
    // @name         Century Tech Solver
    // @namespace    http://tampermonkey.net/
-   // @version      3.12
+   // @version      3.13
    // @description  Auto-solver for Century Tech
    // @author       Funguy
    // @match        https://app.century.tech/*
