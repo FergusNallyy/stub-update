6a7ee40f8c28310f5fe357a0129d02bdeb60142d
Fergus Nally <12160@elizabethcollege.gg>
2026-05-23 00:37:52 +0100

    Update print statement from 'Hello' to 'Goodbye'

--- stub.js
+++ stub.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Century Tech Solver
 // @namespace    http://tampermonkey.net/
-// @version      5.2
+// @version      6.0
 // @description  Auto-solver for Century Tech
 // @author       Funguy
 // @match        https://app.century.tech/*
